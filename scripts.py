15c0e49ae1af1080a380576eb6ad5a6c3e40d458
Denis Tumanov <31006606+dntumanov@users.noreply.github.com>
2021-03-17 10:27:19 +0300

    Update scripts.py

--- scripts.py
+++ scripts.py
@@ -1,17 +1,15 @@
 from datacenter.models import Schoolkid, Mark, Chastisement, Commendation, Lesson
+from django.http import Http404
 
 
 def get_child(full_name: str):
     """Достает из базы нужного ученика"""
     try:
-        Schoolkid.objects.get(full_name__contains=full_name)
+        return Schoolkid.objects.get(full_name__contains=full_name)
     except Schoolkid.MultipleObjectsReturned:
         print('Было найдено больше 1-го ученика с похожим именем. Попробуйте указать имя, фамилию и отчество.')
-        return
     except Schoolkid.DoesNotExist:
         print('Имя указано не верно либо ученика с таким именем/фамилией не существует')
-        return
-    return Schoolkid.objects.get(full_name__contains=full_name)
 
 
 def fix_marks(full_name: str, marks, new_mark: int):
@@ -32,12 +30,15 @@ def remove_chastisements(full_name: str):
 def create_commendation(full_name: str, subject: str, text: str, date: str):
     """Создает замечание для ученика"""
     child = get_child(full_name)
-    lessons = Lesson.objects.filter(subject__title__contains=subject)
-    Commendation.objects.create(
-        text=text,
-        created=date,
-        schoolkid=child,
-        subject=lessons[0].subject,
-        teacher=lessons[0].teacher
-    )
-    print('Сделано. Проверяй')
+    lessons = list(Lesson.objects.filter(subject__title__contains=subject).order_by('subject__title'))
+    if not lessons:
+        raise Http404("Не найден такой урок")
+    else:
+        Commendation.objects.create(
+            text=text,
+            created=date,
+            schoolkid=child,
+            subject=lessons[0].subject,
+            teacher=lessons[0].teacher
+        )
+        print('Сделано. Проверяй')
